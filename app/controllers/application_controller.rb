b23aca6c038c6e3af13e4199e071719decc60c9f
David Patrick <dpatrick@lightspeedsystems.com>
2013-12-30 05:55:15 -0800

    quick fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,4 +51,20 @@ class ApplicationController < ActionController::Base
       false
     end
   end
+
+  def print_stock(item, requested = nil)
+    if item.digital
+      'Digital'
+    elsif requested
+      if requested < item.quantity
+        'In Stock'
+      else
+        'Insufficient Stock'
+      end
+    elsif item.quantity > 0
+      "In Stock: #{item.quantity}"
+    else
+      "Out of Stock"
+    end
+  end
 end
